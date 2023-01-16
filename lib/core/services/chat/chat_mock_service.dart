75d4ce7c15ce43c4870c1855f234f5216ca40c11
Bruno Pansarim <bruno.pansarim@gmail.com>
2023-01-16 20:37:25 -0300

    Resolvido
    Erros tratados

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:math';
 
-import 'package:chatfirebase/components/new_message.dart';
 import 'package:chatfirebase/core/models/chat_message.dart';
 import 'package:chatfirebase/core/models/chat_user.dart';
 import 'package:chatfirebase/core/services/chat/chat_service.dart';
@@ -9,17 +8,17 @@ import 'package:chatfirebase/core/services/chat/chat_service.dart';
 class ChatMockService implements ChatService {
   static final List<ChatMessage> _msgs = [
     ChatMessage(
-        id: '1',
-        text: 'Bom dia',
-        createdAt: DateTime.now(),
-        userId: 'teste',
-        username: 'xD123',
-        userImageURL: 'assets/images/avatar.png',
+      id: '1',
+      text: 'Bom dia',
+      createdAt: DateTime.now().toString(),
+      userId: 'teste',
+      username: 'xD123',
+      userImageURL: 'assets/images/avatar.png',
     ),
     ChatMessage(
       id: '2',
       text: 'Boa tarde',
-      createdAt: DateTime.now(),
+      createdAt: DateTime.now().toString(),
       userId: 'teste1',
       username: 'xD321',
       userImageURL: 'assets/images/avatar.png',
@@ -27,7 +26,7 @@ class ChatMockService implements ChatService {
     ChatMessage(
       id: '3',
       text: 'Boa Noite',
-      createdAt: DateTime.now(),
+      createdAt: DateTime.now().toString(),
       userId: 'teste2',
       username: 'xP123',
       userImageURL: 'assets/images/avatar.png',
@@ -36,9 +35,9 @@ class ChatMockService implements ChatService {
 
   static MultiStreamController<List<ChatMessage>>? _controller;
   static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
-      _controller = controller;
-      controller.add(_msgs);
-});
+    _controller = controller;
+    controller.add(_msgs);
+  });
 
   Stream<List<ChatMessage>> messagesStream() {
     return _msgsStream;
@@ -52,12 +51,12 @@ class ChatMockService implements ChatService {
   @override
   Future<ChatMessage> save(String text, ChatUser user) async {
     final newMessage = ChatMessage(
-          id: Random().nextDouble().toString(),
-          text: text,
-          createdAt: DateTime.now(),
-          userId: user.id,
-          username: user.name,
-          userImageURL: user.imageURL,
+      id: Random().nextDouble().toString(),
+      text: text,
+      createdAt: (''),
+      userId: user.id,
+      username: user.name,
+      userImageURL: user.imageURL,
     );
     _msgs.add(newMessage);
     _controller?.add(_msgs);
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const AuthPage(),
+      home: const AuthOrPage(),
       debugShowCheckedModeBanner: false,
     );
   }
