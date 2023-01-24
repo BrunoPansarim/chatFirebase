fbcca1ffd2be16a3dc8f0cc09511ff9cb77813b1
BrunoPansarim <bruno.pansarim@gmail.com>
2023-01-24 14:59:11 -0300

    ChatFirebase
    update chatfirebase

--- lib/core/services/chat/chat_firebase_.dart
+++ lib/core/services/chat/chat_firebase_.dart
@@ -1,29 +1,84 @@
 import 'dart:async';
-
 import 'package:chatfirebase/core/models/chat_message.dart';
 import 'package:chatfirebase/core/models/chat_user.dart';
 import 'package:chatfirebase/core/services/chat/chat_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class ChatFirebaseService implements ChatService {
-  @override
   Stream<List<ChatMessage>> messagesStream() {
-  return const Stream<List<ChatMessage>>.empty();
+    final store = FirebaseFirestore.instance;
+    final snapshots = store
+        .collection('chat')
+        .withConverter(
+          fromFirestore: _fromFirestore,
+          toFirestore: _toFirestore,
+        )
+        .snapshots();
 
-  }
+    return snapshots.map((snapshot) {
+
+    });
+
+    // return Stream<List<ChatMessage>>.multi((controller) {
+    //   snapshots.listen((snapshot) {
+    //     List<ChatMessage> lista = snapshot.docs.map((doc) {
+    //       return doc.data();
+    //     }).toList();
+    //     controller.add(lista);
+    //   });
+    // });
+    }
 
   @override
   Future<ChatMessage?> save(String text, ChatUser user) async {
     final store = FirebaseFirestore.instance;
-    store.collection('chat').add({
-      'text': text,
-      'createdAt': DateTime.now().toIso8601String(),
-      'userId': user.id,
-      'username': user.name,
-      'userImageURL': user.imageURL,
-    });
 
-    return null;
-    }
+    final msg = ChatMessage(
+      id: '',
+      text: text,
+      createdAt: DateTime.now().toIso8601String(),
+      userId: user.id,
+      username: user.name,
+      userImageURL: user.imageURL,
+    );
+
+    final docRef = await store
+        .collection('chat')
+        .withConverter(
+          fromFirestore: _fromFirestore,
+          toFirestore: _toFirestore,
+        )
+        .add(msg);
+
+    final doc = await docRef.get();
+    return doc.data()!;
   }
 
+  Map<String, dynamic> _toFirestore(
+    ChatMessage msg,
+    SetOptions? options,
+  ) {
+    return {
+      'text': msg.text,
+      'createdAt': DateTime.now().toIso8601String(),
+      'userId': msg.userId,
+      'userName': msg.username,
+      'userImageUrl': msg.userImageURL,
+    };
+  }
+
+  ChatMessage _fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> doc,
+    SnapshotOptions? options,
+  ) {
+    return ChatMessage(
+      id: doc.id,
+      text: doc['text'],
+      createdAt: DateTime.parse(['createdAt']),
+      userId: doc['userId'],
+      username: doc['userName'],
+      userImageURL: doc['userImageUrl'],
+    );
+  }
+}
